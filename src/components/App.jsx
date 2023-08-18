c7954556c21ba33b532153ba97ffdc656e51dc35
Maksim Gulay <maksimgulaj@gmail.com>
2023-08-18 10:21:44 +0300

    work on step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+import { nanoid } from 'nanoid'
+
+  const schema = Yup.object().shape({
+    name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  })
+
+  const addNewContact = newContact => {
+    this.setState(prewState => {
+      return {
+        contacts: [...prewState.contacts, newContact]
+      }
+    })
+    console.log(newContact)
+  }
+ 
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div><Formik
+  initialValues={{
+    contacts: [],
+    name: ''
+  }}
+
+  validationSchema={schema}
+
+  onSubmit={(values, actions) => {
+    addNewContact({...values, id: nanoid()});
+    actions.resetForm();
+  }}
+
+  
+>
+  <Form>
+    <h1>Phonebook</h1>
+    <label> Name</label>
+    <Field id="Name" type="tekst"
+  name="name"
+   />
+    <button type="submit">Add contact</button>
+  </Form>
+</Formik>
+<div>
+  <h2>Contacts</h2>
+  <ul>
+    </ul></div></div>;
 };
